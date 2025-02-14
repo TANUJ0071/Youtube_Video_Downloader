515350db7799dcc5ebd1f66200dc6018d8c860da
TANUJ <106944326+TANUJ0071@users.noreply.github.com>
2025-02-14 14:23:29 +0530

    Update ytdownload.py

--- ytdownload.py
+++ ytdownload.py
@@ -1,1 +1,13 @@
+import yt_dlp
 
+url = "Video_Link_Here"  # Replace with your video URL
+
+
+ydl_opts = {
+    'format': 'best',
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+
+print("Download complete!")
